38c783a363a954307b7f5dfb4b1adf892a7c5446
Virinas-code <alexcode228@gmail.com>
2023-01-11 16:04:19 +0100

    [NETWORK] Rewrite hash using MD5
    Should fix training bug.

--- whiterabbit/neural_network/utils/hash.py
+++ whiterabbit/neural_network/utils/hash.py
@@ -5,33 +5,58 @@ White Rabbit chess engine.
 
 Neural network hash.
 """
+from hashlib import md5
+
 import numpy as np
 
 
-def hash_arrays(arrays: list[np.ndarray]) -> tuple[int]:
+def calc_hash(data: bytes) -> int:
+    """
+    Compute hash of data.
+
+    :param bytes data: Data to compute hash of.
+    :return int: Hashed data.
+    """
+    return int(md5(data).hexdigest(), 16)
+
+
+def list_hash(data: list[int]) -> int:
+    """
+    Compute hash of a list of hashes.
+
+    :param tuple[int] data: List of hashes.
+    :return int: Hashed data.
+    """
+    string: str = ""
+    for element in data:
+        string += str(element)
+    return calc_hash(string.encode())
+
+
+def hash_arrays(arrays: list[np.ndarray]) -> int:
     """
     Hash a list of numpy arrays.
 
     :param list[np.ndarray] arrays: List of numpy arrays.
-    :return tuple[int]: Tuple of hashes.
+    :return tuple[int]: Hash.
     """
     hashes: list[int] = []
     for array in arrays:
-        hashes.append(hash(array.tobytes()))
-    return tuple(hashes)
+        hashes.append(calc_hash(array.tobytes()))
+    return list_hash(hashes)
 
 
-def hash_arrays_dict(arrays_dict: dict[str, np.ndarray]) -> tuple[int]:
+def hash_arrays_dict(arrays_dict: dict[str, np.ndarray]) -> int:
     """
     Hash a dict of numpy arrays.
 
     :param dict[str, np.ndarray] arrays_dict: Dict of numpy arrays.
-    :return tuple[int]: Tuple of hashes.
+    :return int: Hash.
     """
     hashes: list[int] = []
     for key, array in arrays_dict.items():
-        hashes.append(hash((key, array.tobytes())))
-    return tuple(hashes)
+        hashes.append(calc_hash(key.encode() + array.tobytes()))
+    return list_hash(hashes)
 
 
 def network_hash(network) -> int:
@@ -41,13 +66,13 @@ def network_hash(network) -> int:
     :param NeuralNetwork network: Network to get hash of.
     :return int: Network hash.
     """
-    return hash(
-        (
+    return list_hash(
+        [
             hash_arrays(network.matrices_left),
             hash_arrays(network.matrices_right),
             hash_arrays_dict(network.scalar_matrices),
             hash_arrays_dict(network.reduce_matrices),
             hash_arrays(network.biases),
             hash_arrays_dict(network.correction),
-        )
+        ]
     )
