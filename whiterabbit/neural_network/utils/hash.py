4190f2c921326bd242d723c31ecdeae6e789562c
Virinas-code <alexcode228@gmail.com>
2023-01-11 16:31:05 +0100

    [NETWORK] New hash function
    Tested

--- whiterabbit/neural_network/utils/hash.py
+++ whiterabbit/neural_network/utils/hash.py
@@ -7,8 +7,6 @@ Neural network hash.
 """
 from hashlib import md5
 
-import numpy as np
-
 
 def calc_hash(data: bytes) -> int:
     """
@@ -20,45 +18,6 @@ def calc_hash(data: bytes) -> int:
     return int(md5(data).hexdigest(), 16)
 
 
-def list_hash(data: list[int]) -> int:
-    """
-    Compute hash of a list of hashes.
-
-    :param tuple[int] data: List of hashes.
-    :return int: Hashed data.
-    """
-    string: str = ""
-    for element in data:
-        string += str(element)
-    return calc_hash(string.encode())
-
-
-def hash_arrays(arrays: list[np.ndarray]) -> int:
-    """
-    Hash a list of numpy arrays.
-
-    :param list[np.ndarray] arrays: List of numpy arrays.
-    :return tuple[int]: Hash.
-    """
-    hashes: list[int] = []
-    for array in arrays:
-        hashes.append(calc_hash(array.tobytes()))
-    return list_hash(hashes)
-
-
-def hash_arrays_dict(arrays_dict: dict[str, np.ndarray]) -> int:
-    """
-    Hash a dict of numpy arrays.
-
-    :param dict[str, np.ndarray] arrays_dict: Dict of numpy arrays.
-    :return int: Hash.
-    """
-    hashes: list[int] = []
-    for key, array in arrays_dict.items():
-        hashes.append(calc_hash(key.encode() + array.tobytes()))
-    return list_hash(hashes)
-
-
 def network_hash(network) -> int:
     """
     Get a network hash.
@@ -66,13 +25,7 @@ def network_hash(network) -> int:
     :param NeuralNetwork network: Network to get hash of.
     :return int: Network hash.
     """
-    return list_hash(
-        [
-            hash_arrays(network.matrices_left),
-            hash_arrays(network.matrices_right),
-            hash_arrays_dict(network.scalar_matrices),
-            hash_arrays_dict(network.reduce_matrices),
-            hash_arrays(network.biases),
-            hash_arrays_dict(network.correction),
-        ]
-    )
+    all_bytes: bytes = b""
+    for array in network:
+        all_bytes += array.tobytes()
+    return calc_hash(all_bytes)
