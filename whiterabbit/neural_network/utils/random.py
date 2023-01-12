5cdd0e421c708747e4b4c98c0faebace5ab398d2
Virinas-code <alexcode228@gmail.com>
2023-01-12 19:04:20 +0100

    [NETWORK] Custom range in random

--- whiterabbit/neural_network/utils/random.py
+++ whiterabbit/neural_network/utils/random.py
@@ -10,7 +10,7 @@ import numpy as np
 HIDDEN_LAYERS: int = 8
 
 
-def random_method(cls):
+def random_method(cls, maximum: int = 255):
     """
     Generate a random network.
 
@@ -21,27 +21,27 @@ def random_method(cls):
     biases: list[np.ndarray] = []
     for layer in range(HIDDEN_LAYERS + 2):
         matrices_left.append(
-            np.random.randint(0, 255, (8, 8, 12, 12)).astype(np.uint8)
+            np.random.randint(0, maximum, (8, 8, 12, 12)).astype(np.uint8)
         )
         matrices_right.append(
-            np.random.randint(0, 255, (8, 8, 12, 12)).astype(np.uint8)
+            np.random.randint(0, maximum, (8, 8, 12, 12)).astype(np.uint8)
         )
         biases.append(
-            np.random.randint(0, 255, (8, 8, 12, 12)).astype(np.uint8)
+            np.random.randint(0, maximum, (8, 8, 12, 12)).astype(np.uint8)
         )
     scalar_matrices: dict[str, np.ndarray] = {
-        "R-Gi": np.random.randint(0, 255, (8, 8, 1, 12)).astype(np.uint8),
-        "R-Di": np.random.randint(0, 255, (8, 8, 12, 1)).astype(np.uint8),
-        "R-Ge": np.random.randint(0, 255, (1, 8, 1, 1)).astype(np.uint8),
-        "R-De": np.random.randint(0, 255, (8, 1, 1, 1)).astype(np.uint8),
+        "R-Gi": np.random.randint(0, maximum, (8, 8, 1, 12)).astype(np.uint8),
+        "R-Di": np.random.randint(0, maximum, (8, 8, 12, 1)).astype(np.uint8),
+        "R-Ge": np.random.randint(0, maximum, (1, 8, 1, 1)).astype(np.uint8),
+        "R-De": np.random.randint(0, maximum, (8, 1, 1, 1)).astype(np.uint8),
     }
     reduce_matrices: dict[str, np.ndarray] = {
-        "RM-G": np.random.randint(0, 255, (16, 96)).astype(np.uint8),
-        "RM-D": np.random.randint(0, 255, (96, 14)).astype(np.uint8),
+        "RM-G": np.random.randint(0, maximum, (16, 96)).astype(np.uint8),
+        "RM-D": np.random.randint(0, maximum, (96, 14)).astype(np.uint8),
     }
     correction: dict[str, np.ndarray] = {
-        "R-G": np.random.randint(0, 255, (8, 8, 12, 12)).astype(np.uint8),
-        "R-D": np.random.randint(0, 255, (8, 8, 12, 12)).astype(np.uint8),
+        "R-G": np.random.randint(0, maximum, (8, 8, 12, 12)).astype(np.uint8),
+        "R-D": np.random.randint(0, maximum, (8, 8, 12, 12)).astype(np.uint8),
     }
     return cls(
         matrices_left,
