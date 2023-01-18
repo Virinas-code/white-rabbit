dbdf7982db450b0ec73069741ecbfaead71c31cc
Virinas-code <alexcode228@gmail.com>
2023-01-18 12:45:34 +0100

    [TRAINING] Not working :'(

--- whiterabbit/trainer/config.py
+++ whiterabbit/trainer/config.py
@@ -4,9 +4,14 @@ White Rabbit Chess Engine.
 
 Constants config.
 """
-NETWORKS_INDEXES_PLAYING: list[int] = [0, 10, 20, 240, 250, 256]
+NETWORKS_INDEXES_PLAYING: list[int] = [0, 2, 254, 256]
 DEPTHS: list[int] = [3]
 POSITIONS: list[str] = [
     "8/5K1k/8/8/8/8/8/R7 w - - 0 1",
     "1k6/8/1K6/8/8/8/8/7R w - - 0 1",
+    "7R/8/8/4R3/8/1K6/8/k7 w - - 0 1",
+    "8/7k/8/8/1R6/3K4/8/6R1 w - - 0 1",
+    "3R4/6K1/8/8/1k6/8/4R3/8 w - - 0 1",
+    "6k1/3Q4/7K/8/8/8/8/8 w - - 0 1",
 ]
+RANDOM_MAXIMUM: int = 4
